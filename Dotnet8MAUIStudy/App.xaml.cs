3eeca996aaf63a2d0a9cc88457001b08f6965f95
Yuu Eguchi (ICSOFT) <y.eguchi@icsoft.co.jp>
2024-09-21 14:06:23 +0900

    Add project files.

---
+++ Dotnet8MAUIStudy/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Dotnet8MAUIStudy
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Dotnet8MAUIStudy/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Dotnet8MAUIStudy
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
