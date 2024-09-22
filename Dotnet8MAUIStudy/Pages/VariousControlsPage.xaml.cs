3378dc1fe9b9e1d4de594901457a28216ffa73bb
yuu-eguci <yuu.eguci@gmail.com>
2024-09-22 16:05:24 +0900

    ✨ VariousControlsPage 軽く UI を追加

--- Dotnet8MAUIStudy/Pages/VariousControlsPage.xaml.cs
+++ Dotnet8MAUIStudy/Pages/VariousControlsPage.xaml.cs
@@ -2,8 +2,22 @@ namespace Dotnet8MAUIStudy.Pages;
 
 public partial class VariousControlsPage : ContentPage
 {
-	public VariousControlsPage()
-	{
-		InitializeComponent();
-	}
+    public VariousControlsPage()
+    {
+        InitializeComponent();
+    }
+
+    // ウィンドウタイトル変更ボタンが押されたときの処理
+    private void OnChangeTitleClicked(object sender, EventArgs e)
+    {
+        // ウィンドウタイトルを変更
+        this.Title = "タイトルが変更されました";
+    }
+
+    // メッセージボックスボタンが押されたときの処理
+    private async void OnShowMessageBoxClicked(object sender, EventArgs e)
+    {
+        // メッセージボックスを表示
+        await DisplayAlert("メッセージ", "これはメッセージボックスです", "OK");
+    }
 }
