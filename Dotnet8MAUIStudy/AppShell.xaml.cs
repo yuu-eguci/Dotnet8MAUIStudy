12c586484a4cc7df2a2ecfe039c600e59e8db237
yuu-eguci <yuu.eguci@gmail.com>
2024-09-22 13:27:11 +0900

    ✨ AppShell のルーティングとナビゲーションの理解を整理

--- Dotnet8MAUIStudy/AppShell.xaml.cs
+++ Dotnet8MAUIStudy/AppShell.xaml.cs
@@ -6,9 +6,11 @@
         {
             InitializeComponent();
 
-            // ここへ登録すると、 GoToAsync を使ってページ遷移ができる。
-            Routing.RegisterRoute(nameof(Pages.LoginPage), typeof(Pages.LoginPage));
-            Routing.RegisterRoute(nameof(Pages.MainMenuPage), typeof(Pages.MainMenuPage));
+            // NOTE: ここには、ルートから遷移していくページを登録する。
+            //       通常の遷移は `GoToAsync("SamplePage")` でスタックを積んで進んだり、
+            //       `Navigation.PopAsync()` でスタックを除いて戻ったりして行う。
+            //       裏技で、 `GoToAsync("//MainMenuPage")` などで、
+            //       スタックをリセットしてルートのページへ遷移することができる。 (ルート以外は指定不可)
             Routing.RegisterRoute(nameof(Pages.SampleInputPage), typeof(Pages.SampleInputPage));
         }
     }
