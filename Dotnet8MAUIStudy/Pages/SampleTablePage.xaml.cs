318973eaf1823c5b664dd59747c495bc745499b8
yuu-eguci <yuu.eguci@gmail.com>
2024-09-22 13:42:28 +0900

    ✨ SampleTablePage 表以外の UI をざっくりと追加

--- Dotnet8MAUIStudy/Pages/SampleTablePage.xaml.cs
+++ Dotnet8MAUIStudy/Pages/SampleTablePage.xaml.cs
@@ -2,8 +2,22 @@ namespace Dotnet8MAUIStudy.Pages;
 
 public partial class SampleTablePage : ContentPage
 {
-	public SampleTablePage()
-	{
-		InitializeComponent();
-	}
+    public SampleTablePage()
+    {
+        InitializeComponent();
+    }
+
+    // 戻るリンクが押されたときの処理
+    private async void OnBackToMainMenuClicked(object sender, EventArgs e)
+    {
+        // PopAsync で前のページに戻る
+        await Shell.Current.Navigation.PopAsync();
+    }
+
+    // 印刷ボタンが押されたときの処理
+    private async void OnPrintButtonClicked(object sender, EventArgs e)
+    {
+        // 仮の印刷処理
+        await DisplayAlert("印刷", "印刷処理が実行されました！", "OK");
+    }
 }
