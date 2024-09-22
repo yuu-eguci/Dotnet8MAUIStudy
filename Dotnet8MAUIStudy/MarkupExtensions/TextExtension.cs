28cf54524a5cd44b61d8846d19f3f35a8660e8d9
yuu-eguci <yuu.eguci@gmail.com>
2024-09-22 17:10:37 +0900

    ✨ TextExtension xaml から TextConstants を参照できるように

---
+++ Dotnet8MAUIStudy/MarkupExtensions/TextExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Dotnet8MAUIStudy.Helpers;
+
+namespace Dotnet8MAUIStudy.MarkupExtensions
+{
+    // NOTE: 普通に考えると Helpers.TextConstants だけでいいと思うんだけれど、
+    //       xaml のアホは直接 static なクラスを参照できない。
+    //       そのため、IMarkupExtension<T> を使って、 xaml から参照できるようにしているというわけ。
+    public class TextExtension : IMarkupExtension<string>
+    {
+        public string Key { get; set; }
+
+        public string ProvideValue(IServiceProvider serviceProvider)
+        {
+            // TextConstants クラスから、指定された Key に対応するテキストを取得し、返す。
+            return TextConstants.GetText(Key);
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ProvideValue(serviceProvider);
+        }
+    }
+}
