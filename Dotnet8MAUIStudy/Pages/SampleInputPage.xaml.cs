4066053353e20e4ee69f12a3213e975efe57ae38
yuu-eguci <yuu.eguci@gmail.com>
2024-09-25 11:31:29 +0900

    ✨ ご要望: SampleInputPage コード入力 -> 他、自動入力

--- Dotnet8MAUIStudy/Pages/SampleInputPage.xaml.cs
+++ Dotnet8MAUIStudy/Pages/SampleInputPage.xaml.cs
@@ -7,6 +7,34 @@ public partial class SampleInputPage : ContentPage
 		InitializeComponent();
 	}
 
+    // 製品コードが変更されたときの処理
+    private void OnProductCodeChanged(object sender, TextChangedEventArgs e)
+    {
+        var productCode = e.NewTextValue;
+
+        if (productCode == "0001")
+        {
+            ProductNameEntry.Text = "みかん";
+            UnitPriceEntry.Text = "10000";
+        }
+        else if (productCode == "0002")
+        {
+            ProductNameEntry.Text = "デコポン";
+            UnitPriceEntry.Text = "300000";
+        }
+        else if (productCode == "0003")
+        {
+            ProductNameEntry.Text = "甘夏";
+            UnitPriceEntry.Text = "500000";
+        }
+        else
+        {
+            // 無効な製品コードのときは空欄にする
+            ProductNameEntry.Text = string.Empty;
+            UnitPriceEntry.Text = string.Empty;
+        }
+    }
+
     // 登録ボタンが押されたときの処理
     private async void OnRegisterButtonClicked(object sender, EventArgs e)
     {
