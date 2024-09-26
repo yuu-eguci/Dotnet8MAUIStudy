8f522a0e2c7bdb4e6a25770b7ac5b50a9e92e012
yuu-eguci <yuu.eguci@gmail.com>
2024-09-26 20:00:46 +0900

    ✨ ご要望: SampleInputPage 金額を自動計算

--- Dotnet8MAUIStudy/Pages/SampleInputPage.xaml.cs
+++ Dotnet8MAUIStudy/Pages/SampleInputPage.xaml.cs
@@ -2,10 +2,16 @@ namespace Dotnet8MAUIStudy.Pages;
 
 public partial class SampleInputPage : ContentPage
 {
-	public SampleInputPage()
-	{
-		InitializeComponent();
-	}
+    public SampleInputPage()
+    {
+        InitializeComponent();
+
+        // 単価が変更されたときに OnPriceOrQuantityChanged メソッドを呼び出す設定です。
+        UnitPriceEntry.TextChanged += OnPriceOrQuantityChanged;
+
+        // 数量が変更されたときに OnPriceOrQuantityChanged メソッドを呼び出す設定です。
+        QuantityEntry.TextChanged += OnPriceOrQuantityChanged;
+    }
 
     // 製品コードが変更されたときの処理
     private void OnProductCodeChanged(object sender, TextChangedEventArgs e)
@@ -15,23 +21,40 @@ public partial class SampleInputPage : ContentPage
         if (productCode == "0001")
         {
             ProductNameEntry.Text = "みかん";
-            UnitPriceEntry.Text = "10000";
+            UnitPriceEntry.Text = "1000";
         }
         else if (productCode == "0002")
         {
             ProductNameEntry.Text = "デコポン";
-            UnitPriceEntry.Text = "300000";
+            UnitPriceEntry.Text = "3000";
         }
         else if (productCode == "0003")
         {
             ProductNameEntry.Text = "甘夏";
-            UnitPriceEntry.Text = "500000";
+            UnitPriceEntry.Text = "5000";
         }
         else
         {
             // 無効な製品コードのときは空欄にする
             ProductNameEntry.Text = string.Empty;
             UnitPriceEntry.Text = string.Empty;
+        }
+    }
+
+    // 単価、数量が変更されたときの処理
+    private void OnPriceOrQuantityChanged(object sender, TextChangedEventArgs e)
+    {
+        if (decimal.TryParse(UnitPriceEntry.Text, out var unitPrice) &&
+            decimal.TryParse(QuantityEntry.Text, out var quantity))
+        {
+            // 単価 * 数量を計算して金額にセットします。
+            var totalPrice = unitPrice * quantity;
+            AmountEntry.Text = totalPrice.ToString();
+        }
+        else
+        {
+            // 単価か数量が数値でない場合、金額を空欄にします。
+            AmountEntry.Text = string.Empty;
         }
     }
 
